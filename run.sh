ecc71aa551265d3697f38ce1a278866027a300cb
Shiva <shivanandhan33@gmail.com>
2024-12-27 11:31:39 +0530

    New repo

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git pull;
   echo "usage: bash run.sh <component-name> <env>"
   exit 1
  fi
-ansible-playbook -i $1-$2.kndevops72.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e app_name=$1 roboshop.yml
+ansible-playbook -i $1-$2.kndevops72.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e component=$1 roboshop.yml
